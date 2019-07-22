4d9ebc995629e23b0cda386efbe722c6005edd52
Alex Odorico <alex.odorico@morleynet.com>
2019-07-22 16:45:02 -0400

    Have crude matchmaking working

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+let gameId = new Array();
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.get("/joingame", (req, res) => {
+  if (gameId.length < 1) {
+    gameId.push((Math.random() + 1).toString(36).slice(2, 18));
+    res.json({ id: gameId[0] });
+  } else {
+    res.json({ id: gameId[0] });
+    gameId = new Array();
+  }
+
+  const room = io.of(`/${gameId[0]}`);
+  room.on("connection", socket => {
+    console.log(socket.nsp.name);
+    socket.on("action", msg => {
+      console.log("asction");
+      socket.broadcast.emit("action", msg);
+    });
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
